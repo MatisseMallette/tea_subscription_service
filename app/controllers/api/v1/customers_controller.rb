abefb6ef961172df24d8dd2342d12a468cd36400
Matisse Mallette <matissemallette@gmail.com>
2023-03-31 04:49:36 -0400

    Fix ShouldaMatchers.

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::CustomersController < ApplicationController
+  def index
+    render "Hello, world!"
+  end
+end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+  end
+
+  describe 'validations' do
+  end
 end
