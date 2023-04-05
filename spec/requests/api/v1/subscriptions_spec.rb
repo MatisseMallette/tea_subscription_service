d375749de2788b758c6d88f449d2eb88cf15733c
Matisse Mallette <matissemallette@gmail.com>
2023-04-05 07:09:43 -0400

    Create get subscriptions endpoint test.

--- spec/requests/api/v1/subscriptions_spec.rb
+++ spec/requests/api/v1/subscriptions_spec.rb
@@ -5,6 +5,39 @@ RSpec.describe "Api::V1::Subscriptions", type: :request do
   #   pending "add some examples (or delete) #{__FILE__}"
   # end
 
+  describe 'get subscriptions' do
+    it 'can return a list of all subscriptions' do
+      tea1 = Tea.create!(title: 'Tea1', description: 'Tea1 description', temperature: 'Tea1 temperature', brew_time: 'Tea1 brew time')
+      tea2 = Tea.create!(title: 'Tea2', description: 'Tea2 description', temperature: 'Tea2 temperature', brew_time: 'Tea2 brew time')
+      tea3 = Tea.create!(title: 'Tea3', description: 'Tea3 description', temperature: 'Tea3 temperature', brew_time: 'Tea3 brew time')
+      customer = Customer.create!(first_name: 'Jeff', last_name: 'Johnson', email: "jeffjohnson@gmail.com", address: '123 Main Street')
+      subscriptio1 = Subscription.create!(title: 'cool subscription 1', price: 'a lot', status: 'active', frequency: 'daily', tea: tea1, customer: customer)
+      subscription2 = Subscription.create!(title: 'cool subscription 2', price: 'a lot', status: 'active', frequency: 'daily', tea: tea2, customer: customer)
+      subscription3 = Subscription.create!(title: 'cool subscription 3', price: 'a lot', status: 'active', frequency: 'daily', tea: tea3, customer: customer)
+
+      get '/api/v1/subscriptions'
+
+      subscriptions = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(subscriptions).to be_an(Array)
+      expect(subscriptions.length).to eq(3)
+      expect(subscriptions.first.keys).to eq([:id, :type, :attributes, :relationships])
+      expect(subscriptions.first[:attributes].keys).to eq([:title, :price, :status, :frequency])
+
+      subscriptions.each do |subscription|
+        expect(subscription[:id]).to be_a(String)
+        expect(subscription[:type]).to eq('subscription')
+        expect(subscription[:attributes][:title]).to be_a(String)
+        expect(subscription[:attributes][:price]).to be_a(String)
+        expect(subscription[:attributes][:status]).to be_a(String)
+        expect(subscription[:attributes][:frequency]).to be_a(String)
+      end
+
+      expect(subscriptions.first[:attributes][:title]).to eq(Subscription.first.title)
+      expect(subscriptions.last[:attributes][:title]).to eq(Subscription.last.title)
+    end
+  end
+
   describe 'post subscription' do
     it 'can create a new subscription' do
       customer1 = Customer.create!(first_name: 'Jeff', last_name: 'Johnson', email: "jeffjohnson@gmail.com", address: '123 Main Street')
