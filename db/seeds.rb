bc6df434c298b1f4afd05f1eef4b0d26cad7a046
Matisse Mallette <matissemallette@gmail.com>
2023-04-05 04:27:16 -0400

    Create tea index endpoint.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tea.create(title: 'Tea1', description: 'Tea1 description', temperature: 'Tea1 temperature', brew_time: 'Tea1 brew time')
+      Tea.create(title: 'Tea2', description: 'Tea2 description', temperature: 'Tea2 temperature', brew_time: 'Tea2 brew time')
+      Tea.create(title: 'Tea3', description: 'Tea3 description', temperature: 'Tea3 temperature', brew_time: 'Tea3 brew time')
--- spec/requests/api/v1/teas_spec.rb
+++ spec/requests/api/v1/teas_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Teas", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # describe "GET /index" do
+  #   pending "add some examples (or delete) #{__FILE__}"
+  # end
+
+  describe 'get teas' do
+    it 'can return a list of all teas' do 
+      Tea.create(title: 'Tea1', description: 'Tea1 description', temperature: 'Tea1 temperature', brew_time: 'Tea1 brew time')
+      Tea.create(title: 'Tea2', description: 'Tea2 description', temperature: 'Tea2 temperature', brew_time: 'Tea2 brew time')
+      Tea.create(title: 'Tea3', description: 'Tea3 description', temperature: 'Tea3 temperature', brew_time: 'Tea3 brew time')
+      get '/api/v1/teas'
+      
+      expect(response).to be_successful
+
+      teas = JSON.parse(response.body, symbolize_names: true)[:data]
+      
+      expect(teas).to be_an(Array)
+
+      binding.pry
+    end
   end
 end
